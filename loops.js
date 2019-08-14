f35a7de9efe5d82b7421c2e05a73ec017b971e3f
Nivetha <nivedha.orton@gmail.com>
2019-08-14 08:07:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,13 @@
 
 function forLoop(array){
-for(var i=1;i<25;i++){
-  console.log(i===1?"I am 1 strange loop.":`I am ${i} strange loops.`)
+for(Let i=1;i<25;i++){
+  if(i===1){
+    array.push("I am 1 strange loop.")
+  }else{
+    array.push(`I am ${i} strange loops.`)
+  }
+  
 
-}
+}return array
 
 }
